461e7b4eb8a368a9800fe1aa1adff7703a7c5477
Guiennet <bilbo0o0the0o0hobbit@gmail.com>
2018-11-19 08:37:47 +0100

    add a way to stop the simulation and go back to edit mode, also add a way too clear the screen and toogle a cell

--- interface.py
+++ interface.py
@@ -4,39 +4,44 @@ import numpy as np
 from jeuDeLaVie import World
 from time import sleep
 
+SHOWN_CHARACTER = '.'
 def draw_menu(stdscr):
     k = 0
-    cursor_x = 0
-    cursor_y = 0
+    height, width = stdscr.getmaxyx()
+    cursor_x = width//2 - 1
+    cursor_y = height//2 - 1
     curses.start_color()
 
     # Clear and refresh the screen for a blank canvas
     stdscr.clear()
     stdscr.refresh()
 
-    # Start colors in curses
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-
-    height, width = stdscr.getmaxyx()
     cases = np.zeros([height,width],dtype=bool)
     # Loop where k is the last character pressed
     while (k != ord('q')):
 
         if k == ord('\n'):
+            curses.curs_set(0)
             for i in range(width):
                 for j in range(height):
-                    cases[j,i] = stdscr.inch(j,i) & 0xff == ord('0')
+                    cases[j,i] = stdscr.inch(j,i) & 0xff == ord(SHOWN_CHARACTER)
             w=World.from_np_array(cases)
             it = iter(w)
-            while True:
+            k2 = -1 
+            stdscr.nodelay(True)
+            while k2 != ord('q'):
                 stdscr.clear()
                 next(it)
                 for i in range(width):
                     for j in range(height):
                         if w.cases[j,i]:
-                            stdscr.addstr(j,i,'0')
+                            stdscr.addstr(j,i,SHOWN_CHARACTER)
+                k2 = stdscr.getch()
                 stdscr.refresh()
                 sleep(0.1)
+            stdscr.nodelay(False)
+
+        curses.curs_set(2)
 
         # Initialization
         # stdscr.clear()
@@ -57,8 +62,16 @@ def draw_menu(stdscr):
         cursor_y = min(height-1, cursor_y)
 
         if k == ord(' '):
-            stdscr.addstr(cursor_y,cursor_x,'0')
+            if stdscr.inch(cursor_y, cursor_x) & 0xff == ord(SHOWN_CHARACTER):
+                stdscr.addstr(cursor_y,cursor_x,' ')
+            else:
+                stdscr.addstr(cursor_y,cursor_x,SHOWN_CHARACTER)
+
+        elif k == 263:
+            stdscr.clear()
+
         stdscr.move(cursor_y, cursor_x)
+
 
         # Refresh the screen
         stdscr.refresh()
